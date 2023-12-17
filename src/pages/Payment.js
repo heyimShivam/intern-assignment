3209c6531df5760c6002e2bfe6592f737381a885
Shivam <shivamthalwal@gmail.com>
2023-12-17 14:24:59 +0530

    payment options page created

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,11 +1,25 @@
+import { Link } from "react-router-dom";
+
+import PaymentOptions from "../components/PaymentOptions";
+
 import "./Payment.css";
 
 const PaymentPage = () => {
   return (
     <>
       <div className="container">
-        <div>
-          <h3 className="heading">Choose Payment Method</h3>
+        <h3 className="heading">Choose Payment Method</h3>
+        <PaymentOptions />
+        <div className="make-payment-section">
+          <p className="admin-fee-make-payment-section payment-details">
+            Admin Fee <span>{(0).toFixed(3)}</span>
+          </p>
+          <p className="total-fee-make-payment-section payment-details">
+            Total <span>{(5000).toFixed(3)}</span>
+          </p>
+          <Link className="make-payment-btn-inside" to="/payment">
+            Make a payment
+          </Link>
         </div>
       </div>
     </>
